b56abccba8105bb9c9f5d0c8e2b5a338d517bbf3
Jam-Piodos <20221071@nbsc.edu.ph>
2025-02-28 10:50:08 +0800

    added Events initial features

--- src/pages/home-tabs/Events.tsx
+++ src/pages/home-tabs/Events.tsx
@@ -1,14 +1,31 @@
-import { 
+import {
+  IonAvatar,
   IonButtons,
-    IonContent, 
-    IonHeader, 
-    IonMenuButton, 
-    IonPage, 
-    IonTitle, 
-    IonToolbar 
+  IonContent,
+  IonHeader,
+  IonInfiniteScroll,
+  IonInfiniteScrollContent,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonMenuButton,
+  IonPage,
+  IonTitle,
+  IonToolbar
 } from '@ionic/react';
+import { useEffect, useState } from 'react';
 
 const Feed: React.FC = () => {
+  const [items, setItems] = useState<string[]>([]);
+
+  useEffect(() => {
+    const newItems = [];
+    for (let i = 1; i < 51; i++) {
+      newItems.push(`Item ${items.length + i}`);
+    }
+    setItems([...items, ...newItems]);
+  }, []);
+
   return (
     <IonPage>
       <IonHeader>
@@ -20,18 +37,20 @@ const Feed: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <div
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            height: '100%',
-          }}
-        >
-          Upcoming Events
-        </div>
+        No list yet
+
+        <IonList>
+          {items.map((item, index) => (
+            <IonItem key={item}>
+              <IonLabel>{item}</IonLabel>
+            </IonItem>
+          ))}
+        </IonList>
+        <IonInfiniteScroll>
+          <IonInfiniteScrollContent loadingText="Please wait..." loadingSpinner="bubbles"></IonInfiniteScrollContent>
+        </IonInfiniteScroll>
       </IonContent>
-    </IonPage>
+    </IonPage >
   );
 };
 
